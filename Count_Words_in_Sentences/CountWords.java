e231c0acd8c73a931295326971eb9e7e98db5a16
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-05-15 23:50:49 -0400

    update

--- Count_Words_in_Sentences/CountWords.java
+++ Count_Words_in_Sentences/CountWords.java
@@ -3,15 +3,23 @@ import java.util.*;
   
 public class CountWords {
 public static void main(String[] args) throws Exception{
+  HashMap<String, Integer> map = new HashMap<>();
   String[] words = {"first" , "second" , "third" , "fourth"}; 
-  String[] sentences = {"here is my first sentense to test." , "here is the third to count" , "here is nothing" , " consider edge case  with double space then select first.", "second was not included" , "  why don't wecount all words as  well"};
- // int j = 0;
+  String[] sentences = {"here is my first sentense First to test." , "here is the third to count" , "here is nothing" , " consider edge case  with double space then select first.", "second was not included" , "  why don't wecount all words as  well"};
   for (int j = 0 ; j < words.length ; ++j)
   {
+    int c = 0;
     for (int i = 0 ; i < sentences.length ; ++i) {
       if (sentences[i].contains(words[j])) {
-       // ++j;
-        System.out.println(words[j]  + "Found");
+        //System.out.println(words[j]  + "Found");
+        ++c;
+        map.put(words[j],c);
       } 
+    }}
+    for (int o = 0 ; o < words.length ; ++o){
+         if (map.containsKey(words[o])) {
+         System.out.println("The String " + words[o] + " was found " + map.get(words[o]) + " times");
+         //System.out.println(map.get(words[o]));
+         //System.out.println(map.get(words[o]));
+       }
     }}}
-}
---
+++ HashMap/Hash.java~
@@ -0,0 +1,22 @@
+  package HashMap;
+  import java.util.*;
+  
+  public class Hash {
+     public static void main(String[] args) {
+       
+       HashMap<String, Integer> map = new HashMap<>();
+       map.put("v",10);
+       map.put("z",120);
+       map.put("t",130);
+       map.put("r",140);
+       System.out.println(map);
+       int a = map.get("t");
+       System.out.println(a);
+       if (map.containsKey("r")) {
+         System.out.println(map.get("r"));
+       }
+       map.clear();
+       if (map.isEmpty()) {
+        System.out.println("Empty");
+       }}
+     }
