86b233be96c64ecca39861b317479db0b07a4f7e
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-05-15 23:34:33 -0400

    Almost

--- Count_Words_in_Sentences/CountWords.java
+++ Count_Words_in_Sentences/CountWords.java
@@ -5,10 +5,13 @@ public class CountWords {
 public static void main(String[] args) throws Exception{
   String[] words = {"first" , "second" , "third" , "fourth"}; 
   String[] sentences = {"here is my first sentense to test." , "here is the third to count" , "here is nothing" , " consider edge case  with double space then select first.", "second was not included" , "  why don't wecount all words as  well"};
-
-  for (int i = 0 ; i < sentences[0].length() ; ++i) {
-      char current = sentences[0].charAt(i);
-  System.out.println(current);
-
-  }}
+ // int j = 0;
+  for (int j = 0 ; j < words.length ; ++j)
+  {
+    for (int i = 0 ; i < sentences.length ; ++i) {
+      if (sentences[i].contains(words[j])) {
+       // ++j;
+        System.out.println(words[j]  + "Found");
+      } 
+    }}}
 }
