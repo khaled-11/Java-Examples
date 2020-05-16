b8cdfc96b61f819175b51be550e97cba4314d52b
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-05-16 00:12:32 -0400

    Lower

--- Count_Words_in_Sentences/CountWords.java
+++ Count_Words_in_Sentences/CountWords.java
@@ -5,15 +5,16 @@ public class CountWords {
 public static void main(String[] args) throws Exception{
   HashMap<String, Integer> map = new HashMap<>();
   String[] words = {"first" , "second" , "third" , "fourth"}; 
-  String[] sentences = {"here is my first sentense First to test." , "here is the third to count" , "here is nothing" , " consider edge case  with double space then select first.", "second was not included" , "  why don't wecount all words as  well"};
-  String test = "THJI";
-  String lower = lower(test);
-  System.out.println(lower);
+  String[] sentences = {"here is my first First sentense First to test." , "First here is the third to count" , "here First is nothing" , " consider edge case  with double space then select first.", "second was not included" , "  why don't wecount all words as  well"};
+  String test = "THJhhjfHyyI";
+  String lower1 = lower(test);
+  System.out.println(lower1);
   for (int j = 0 ; j < words.length ; ++j)
   {
     int c = 0;
     for (int i = 0 ; i < sentences.length ; ++i) {
-      if (sentences[i].contains(words[j])) {
+      String lower = lower(words[j]);
+      if (sentences[i].contains(words[j]) || sentences[i].contains(lower)) {
         //System.out.println(words[j]  + "Found");
         ++c;
         map.put(words[j],c);
@@ -31,14 +32,22 @@ public static String lower(String str){
   char current;
   String lower ="";
   int tmp;
+  boolean a;
   for ( int k = 0 ; k < str.length() ; ++k){
     current = str.charAt(k);
+    if (current >= 'A' && current <= 'Z')  {
     tmp = (int) current;
     tmp += 32;
     current = (char) tmp;
     lower += current;
+    }
+    else{
+      lower += current;
+    }
   }
   return lower;
 }
+
+
 
 }
