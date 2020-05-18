f51aafc67823eb96808948ab5cb52456dfbc4439
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-05-18 14:35:34 -0400

    .

--- Count_Words_in_Sentences/CountWords.java
+++ Count_Words_in_Sentences/CountWords.java
@@ -2,6 +2,15 @@ package Count_Words_in_Sentences;
 import java.util.*;
   
 public class CountWords {
+  
+  // Global Variables to store the Highest Two.
+  static class global
+{
+    static String FirstMax = null; 
+    static String SecondMax = null; 
+}
+  
+  
 public static void main(String[] args) throws Exception{
   HashMap<String, Integer> map = new HashMap<>();
   Scanner s = new Scanner(System.in); 
@@ -14,12 +23,14 @@ public static void main(String[] args) throws Exception{
     words[k] = lower(input.next());
   }
 
-  String[] sentences = new String [4];
+  String[] sentences = new String [6];
 
     sentences[0] = " This is my FIrST capital Test";
     sentences[1] = "try second fiRst here";
-    sentences[2] = "First talk to first";
+    sentences[2] = "First talk third to first";
     sentences[3] = "only third with first";
+    sentences[4] = "First talk third to first";
+    sentences[5] = "only second with third";
   
   for (int j = 0 ; j < words.length ; ++j)
   {
@@ -36,9 +47,21 @@ public static void main(String[] args) throws Exception{
     for (int o = 0 ; o < words.length ; ++o){
          if (map.containsKey(words[o])) {
          System.out.println("The String " + words[o] + " was found " + map.get(words[o]) + " times");
-       }
-    }}
-
+         }
+    }
+        String max1 = words[3];
+        String max2 = words[3];
+    for (int o = 0 ; o < words.length ; ++o){
+         if (map.get(words[o]) > map.get(max1)) {
+           max1 = words[o];
+         }
+         if (map.get(words[o]) != map.get(max1) && map.get(words[o]) >= map.get(max2)) {
+           max2 = words[o];
+         }
+    }
+    System.out.println("The Max String " + max1 + " was found " + map.get(max1) + " times");
+    System.out.println("The Max2 String " + max2 + " was found " + map.get(max2) + " times");
+}
 public static String lower(String str){
   char current;
   String lower ="";
@@ -57,5 +80,4 @@ public static String lower(String str){
     }
   }
   return lower;
-}
-}
+}}   
